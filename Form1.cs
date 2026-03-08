62ee96704efa005ac907553aa7c68a825ec29a6c
user <user@DESKTOP-9R1VETE>
2026-03-09 00:38:14 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace CS_Week01_25017084_MyBusinessCard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
